3df9e4ee942d5e2e2ba8406e23ecb4db8099e022
Ivan <37061504+ivankuzin@users.noreply.github.com>
2018-03-05 10:53:29 +0300

    Add files via upload

---
+++ HTML/js/script.js
@@ -0,0 +1,23 @@
+//Скрипт для формы обратной связи
+
+$(document).ready(function() {
+
+    $(".popup").magnificPopup();
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
